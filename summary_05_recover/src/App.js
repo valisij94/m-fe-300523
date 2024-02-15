76a96e349ca23b38fbb62391eb0a1d2233f67aa2
Davy <davyd.dabahian@cheetahdigital.com>
2024-02-16 00:25:25 +0200

    summary_05 recovery

--- summary_05_recover/src/App.js
+++ summary_05_recover/src/App.js
@@ -4,8 +4,6 @@ import Home from './pages/Home';
 import LoginPage from './pages/LoginPage';
 import RegisterPage from './pages/RegisterPage';
 import { Link, Route, Routes } from 'react-router-dom';
-import PostsPage from './pages/PostsPage';
-import PostDetailPage from './pages/PostDetailPage';
 
 function App() {
 
@@ -18,13 +16,10 @@ function App() {
             <li><Link to="/">Home</Link></li>
             <li><Link to="/login">Login</Link></li>
             <li><Link to="/register">Register</Link></li>
-            <li><Link to="/posts">Posts</Link></li>
           </ul>
         </nav>
         <Routes>
           <Route path="/" element={<Home />} />
-          <Route path="/posts" element={<PostsPage />} />
-          <Route path="/posts/:postId" element={<PostDetailPage />} />
           <Route path="/login" element={<LoginPage />} />
           <Route path="/register" element={<RegisterPage />} />
           <Route path="*" element={<h3>Not found</h3>} />
--- summary_05_recover/src/redux/store.js
+++ summary_05_recover/src/redux/store.js
@@ -1,14 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 
 import userReducer from  './slices/userSlice.js';
-import postsReducer from './slices/postsSlice.js';
-import { postsApi } from "./slices/apiSlice.js";
 
 export const store = configureStore({
   reducer: {
     user: userReducer,
-    posts: postsReducer
   },
-  middleware: (getDefaultMiddleware) =>
-    getDefaultMiddleware().concat(postsApi.middleware),
 });
